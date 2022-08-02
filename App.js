6d682bf83795d288a2b776cb23b0ea5fb27687d0
johnbuenodev <johnbuenodev@gmail.com>
2022-08-02 19:23:11 -0300

    Exemplo de componente classe com constructor, metodo bind, state, input de dados.

--- App.js
+++ App.js
@@ -1,19 +1,92 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { Component } from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
-export default function App() {
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      nome: '',
+      input: ''
+    };
+
+    //this.pegaNome = this.pushNome.bind(this);
+    this.entrar = this.entrar.bind(this);
+ 
+  }
+
+  pushNome(value) {
+    this.setState({nome: value});
+
+  }
+
+  entrar() {
+
+    if(this.state.input === '') {
+      this.setState({nome: ''});
+      alert("Informe o Login!");
+      return;
+    }
+
+    if(this.state.input) {
+      this.setState({nome: this.state.input});
+    }
+
+  }
+    
+  render() {
   return (
     <View style={styles.container}>
+      <TextInput 
+        style={styles.input}
+        placeholder="Digite o seu Nome..."
+        underlineColorAndroid="transparent"
+        onChangeText={(value) => this.setState({input:value})}
+      />
+
+      <Button title='Entrar' onPress={this.entrar} />
+
+      {
+         this.state.nome ? 
+         <Text style={styles.text}>Bem vindo {this.state.nome}</Text> :
+         <></>
+      } 
       
     </View>
-  );
+   );
+  }
+
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  input: {
+    height: 45,
+    borderWidth: 1,
+    borderColor: '#222',
+    margin: 12,
+    fontSize: 20,
+    padding: 12,
+  },
+  text: {
+    textAlign: 'center',
+    fontSize: 25
+  }
 });
+
+export default App;
+
+/*
+
+<TextInput 
+        style={styles.input}
+        placeholder="Digite o seu Nome..."
+        underlineColorAndroid="transparent"
+        onChangeText={this.getNome}
+      />
+
+*/
